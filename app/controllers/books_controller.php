8efcf9c93dd54032f055abf55b01bae18c344b18
Eric Jiang <erjiang@indiana.edu>
2010-07-03 13:09:00 -0400

    Added some basic configuration things, including database schema. Added the basic code for uploading a PDF (Book).

--- app/controllers/books_controller.php
+++ app/controllers/books_controller.php
@@ -18,10 +18,69 @@ class BooksController extends AppController {
 
 	function add() {
 		if (!empty($this->data)) {
+			/*
+			 * Array
+             * (
+             *     [Book] => Array
+             *         (
+             *             [title] => eou
+             *             [editor] => oeuaoeu
+             *             [publisher] => aoeuaoeu
+             *             [access] => 
+             *             [pdf] => Array
+             *                 (
+             *                     [name] => schema__1_.pdf
+             *                     [type] => application/pdf
+             *                     [tmp_name] => /tmp/php5DNmT0
+             *                     [error] => 0
+             *                     [size] => 67506
+             *                 )
+             *         )
+			 * )
+			echo '<pre>';
+			echo APP . "\n";
+			print_r($this->data);
+			echo '</pre>';
+			 */
+			/*
+			 * Some basic sanity checks on the input
+			 */
+			if($this->data['Book']['pdf']['size'] == 0) {
+				$this->Session->setFlash(__('No PDF was uploaded', true));
+				return;
+			}
+			if($this->data['Book']['pdf']['type'] != "application/pdf") {
+				$this->Session->setFlash(__('Uploaded file is not a PDF', true));
+				return;
+			}
+			/*
+			 * The full filename of the PDF shall be built using its md5sum
+			 * If the file's md5sum is a1b2c3, then its path is
+			 *     PDF_STORE/a/a1b2c3.pdf
+			 */
+			$md5sum = md5_file($this->data['Book']['pdf']['tmp_name']);
+			$initial = substr($md5sum, 0, 1);
+			$fullname = sprintf('%s%s/%s.pdf', PDF_STORE, $initial, $md5sum);
+			if(file_exists(PDF_STORE . $initial . '/' . $md5sum . '.pdf')) {
+				$this->Session->setFlash(__('This file already exists', true));
+				return;
+			}
+
+			$this->data['Book']['filename'] = $md5sum;
 			$this->Book->create();
 			if ($this->Book->save($this->data)) {
+				/*
+				 * If the Book save was successful, process the PDF file as well
+				 */
+				if(!file_exists(PDF_STORE . $initial)) {
+					mkdir(PDF_STORE . $initial);
+				}
+				rename($this->data['Book']['pdf']['tmp_name'], $fullname);
+				/*
+				 * Push the user on to define scores within the book
+				 */
 				$this->Session->setFlash(__('The book has been saved', true));
-				$this->redirect(array('action' => 'index'));
+				$this->redirect(array('action' => 'define'));
 			} else {
 				$this->Session->setFlash(__('The book could not be saved. Please, try again.', true));
 			}
@@ -51,7 +110,11 @@ class BooksController extends AppController {
 			$this->Session->setFlash(__('Invalid id for book', true));
 			$this->redirect(array('action'=>'index'));
 		}
+		$book = $this->Book->read(null, $id);
+		$md5sum = $book['Book']['filename'];
+		$initial = substr($md5sum, 0, 1);
 		if ($this->Book->delete($id)) {
+			unlink(sprintf("%s%s/%s.pdf", PDF_STORE, $initial, $md5sum));
 			$this->Session->setFlash(__('Book deleted', true));
 			$this->redirect(array('action'=>'index'));
 		}
