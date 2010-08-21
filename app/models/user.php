7da5ff8ff067f5f2c6778a4476a66b1e3c53040b
erjiang <erjiang@indiana.edu>
2010-08-21 01:26:49 -0400

    Added validation criteria to users model

--- app/models/user.php
+++ app/models/user.php
@@ -2,5 +2,47 @@
 class User extends AppModel {
 	var $name = 'User';
 	var $displayField = 'name';
+	var $validate = array(
+		'name' => array(
+			'alphanumeric' => array(
+				'rule' => array('alphanumeric'),
+				'message' => 'Username must only have letters and numbers',
+				'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			)
+		),
+		'password' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'email' => array(
+			'email' => array(
+				'rule' => array('email'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'level' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
 }
 ?>
