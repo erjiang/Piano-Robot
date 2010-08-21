adb7a0bafa9248547604faa49e7a3e1a0eadb18a
erjiang <erjiang@indiana.edu>
2010-08-21 00:27:06 -0400

    Fleshed out user creation

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -4,6 +4,7 @@ class UsersController extends AppController {
 	var $name = 'Users';
 
 	function index() {
+		$this->UserLevel->requireLevel(10);
 		$this->User->recursive = 0;
 		$this->set('users', $this->paginate());
 	}
@@ -18,6 +19,27 @@ class UsersController extends AppController {
 
 	function add() {
 		if (!empty($this->data)) {
+			/*
+			 *  SHA1 password
+			 */
+			$this->data['User']['password'] =
+				sha1(
+					$this->data['User']['password'] .
+					Configure::read('Security.salt'));
+
+			/*
+			 * Don't let random people set their own userlevels
+			 */
+			if($this->UserLevel->getLevel() === false ||
+				$this->UserLevel->getLevel() < 10) {
+					$this->data['User']['level'] = 1;
+			}
+
+			/*
+			 * Set the creation date
+			 */
+			$this->data['User']['created'] = date('Y-m-d H:i:s');
+
 			$this->User->create();
 			if ($this->User->save($this->data)) {
 				$this->Session->setFlash(__('The user has been saved', true));
@@ -28,12 +50,68 @@ class UsersController extends AppController {
 		}
 	}
 
+	function login() {
+		if(!empty($this->data)) {
+			$successful = false;
+
+			$found = $this->User->find('first', array(
+				'conditions' => array('User.name' => $this->data['User']['name'])));
+			if($found) {
+				if(sha1($this->data['User']['password'] .
+					Configure::read('Security.salt')) ==
+					$found['User']['password']) {
+						$successful = true;
+
+						/*
+						 * Write the user's last active time
+						 */
+						$found['User']['last_active'] = date('Y-m-d H:i:s');
+						$this->User->save($found);
+
+						/*
+						 * Write the user's access information to Session
+						 */
+						$this->Session->write('User.id', $found['User']['id']);
+						$this->Session->write('User.level', $found['User']['level']);
+
+						/*
+						 * Possibly redirect to the page the user last
+						 * attempted to access
+						 */
+						if($this->Session->check('Auth.redirect')) {
+							$this->flash("Welcome, ".$found['User']['name'].".",
+								$this->Session->read('Auth.redirect'));
+							$this->Session->delete('Auth');
+						}
+						else {
+							$this->flash("Welcome, ".$found['User']['name'].".",
+								'/books/', 3/*seconds*/);
+						}
+					}
+			}
+			if(!$successful) {
+				$this->Session->setFlash("Invalid username or password.");
+			}
+		}
+	}
+
+	function logout() {
+		$this->Session->delete('User');
+		$this->Session->setFlash('You have been logged out.');
+		$this->redirect('/');
+	}
+
 	function edit($id = null) {
 		if (!$id && empty($this->data)) {
 			$this->Session->setFlash(__('Invalid user', true));
 			$this->redirect(array('action' => 'index'));
 		}
 		if (!empty($this->data)) {
+			// SHA1 password
+			$this->data['User']['password'] =
+				sha1(
+					$this->data['User']['password'] .
+					Configure::read('Security.salt'));
 			if ($this->User->save($this->data)) {
 				$this->Session->setFlash(__('The user has been saved', true));
 				$this->redirect(array('action' => 'index'));
